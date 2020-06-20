592450e7709ce13324eec416157febbdc94eaccb
JoseanJM <joseanjmnz.22@gmail.com>
2020-06-20 12:52:50 +0200

    añadimos clase Inventario para almacenar objetos Producto

---
+++ src/Almacen/Inventario.java
@@ -0,0 +1,30 @@
+package Almacen;
+
+import java.util.ArrayList;
+
+public class Inventario {
+
+	private ArrayList<Producto> productos;
+
+	public Inventario() {
+
+		this.productos = new ArrayList<Producto>();
+
+	}
+
+	public void addProd(Producto p) {
+		this.productos.add(p);
+	}
+
+	public int getInvSize() {
+		return this.productos.size();
+	}
+	
+	public void removeProd(Producto p) {
+		this.productos.remove(p);
+	}
+	public boolean checkExists(Producto p) {
+		return this.productos.contains(p);
+	}
+
+}
